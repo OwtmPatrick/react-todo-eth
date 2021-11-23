d1159b88e9f801b274b010188ab9a0c477d9704e
Radik Zangirov <owtmpatrick@gmail.com>
2021-11-23 22:15:05 +0500

    init

---
+++ src/contracts/TodoList.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	uint taskCount = 0;
+	mapping(uint => Task) public tasks;
+
+	constructor() public {
+		createTask("Make todolist blockchain app");
+	}
+
+	event TaskCreated(
+		uint id,
+		string content,
+		bool completed
+	);
+
+	event TaskCompleted(
+		uint id,
+		bool completed
+	);
+
+	function getTasksCount() public view returns (uint) {
+		return taskCount;
+	}
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+
+		emit TaskCreated(taskCount, _content, false);
+	}
+
+	function toggleCompleted(uint _id) public {
+		Task memory _task = tasks[_id];
+		_task.completed = !_task.completed;
+		tasks[_id] = _task;
+
+		emit TaskCompleted(_id, _task.completed);
+	}
+}
