08dc9f70a2317ab6d71bccd1d5ce2e27064c90bf
Radik Zangirov <owtmpatrick@gmail.com>
2021-11-30 19:09:02 +0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import Web3 from 'web3';
+import {Stack, IStackStyles, IStackTokens} from '@fluentui/react/lib/Stack';
+import {DefaultPalette} from '@fluentui/react/lib/Styling';
 import TodoList from './build/contracts/TodoList.json';
+import Account from './components/account';
+import AddTodo from './components/add-todo';
+import List from './components/todolist';
+import './App.css';
 
-console.log(TodoList.abi, Object.values(TodoList.networks)[0].address);
+const stackStyles: IStackStyles = {
+	root: {
+		background: DefaultPalette.themeTertiary,
+		height: '100%'
+	}
+};
+
+const addTodoStyles: IStackStyles = {
+	root: {
+		marginTop: '50px !important',
+		marginBottom: '20px'
+	}
+};
+
+const itemAlignmentsStackTokens: IStackTokens = {
+	padding: 16
+};
+
+const centerStackTokens: IStackTokens = {
+	childrenGap: 24
+};
+
+interface ITask {
+	id: string;
+	content: string;
+	completed: boolean;
+}
 
 function App() {
+	const [account, setAccount] = useState<string | null>(null);
+	const [balance, setBalance] = useState<number | string>(0);
+	const [tasks, setTasks] = useState<ITask[]>([]);
+
+	useEffect(() => {
+		const loadBlockchainData = async () => {
+			getAccount();
+			getTasks();
+		};
+
+		loadBlockchainData();
+	}, []);
+
+	const getAccount = async () => {
+		const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+
+		const accounts = await web3.eth.getAccounts();
+		const account = accounts[0];
+		const balance = web3.utils.fromWei(await web3.eth.getBalance(account), 'ether');
+		setAccount(accounts[0]);
+		setBalance(balance);
+	};
+
+	const getContract = async () => {
+		const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+		const abi: any = TodoList.abi;
+		const lastNetworkIndex: number = Object.values(TodoList.networks).length - 1;
+		const address = Object.values(TodoList.networks)[lastNetworkIndex].address;
+
+		return new web3.eth.Contract(abi, address);
+	};
+
+	const getTasks = async () => {
+		const todoList = await getContract();
+		const taskCount = await todoList.methods.getTasksCount().call();
+		const tasks = [];
+
+		for (let i = 1; i <= taskCount; i++) {
+			const task = await todoList.methods.tasks(i).call();
+
+			tasks.push(task);
+		}
+
+		setTasks(tasks);
+	};
+
+	const addTodo = async (task: any) => {
+		const todoList = await getContract();
+		
+		await todoList.methods.createTask(task).send({from: account});
+		await getTasks();
+	};
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Stack styles={stackStyles} tokens={itemAlignmentsStackTokens}>
+			<Stack.Item align="start">
+				<Account account={account} balance={balance} />
+			</Stack.Item>
+
+			<Stack.Item align="center" styles={addTodoStyles}>
+				<Stack tokens={centerStackTokens}>
+					<Stack.Item align="start">
+						<AddTodo addTodo={addTodo} />
+					</Stack.Item>
+
+					<Stack.Item align="start">
+						<List tasks={tasks} />
+					</Stack.Item>
+				</Stack>
+			</Stack.Item>
+		</Stack>
 	);
 }
 
